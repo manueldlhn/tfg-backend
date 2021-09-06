4fa7737a1de225d943149bd0146d52f9741ddabe
manueldlhn <loloqwerty1@gmail.com>
2021-09-06 19:13:40 +0200

    06/09
    Comprobación en register.js de si el usuario existe antes de crearlo.

--- app/authentication/controllers/register.js
+++ app/authentication/controllers/register.js
@@ -29,29 +29,44 @@ module.exports = (req,res) => {
         ok: false,
     };
 
-    // Contraseña cifrada
-    const hash = bcrypt.hashSync(req.body.Password, 10);
 
-    // Objeto usuario que se va a introducir en la bbdd
-    const userGiven = {
-            Email: req.body.Email,
-            Password: hash,
-            Nombre: req.body.Nombre,
-            Fecha_Nacimiento: req.body.Fecha_Nacimiento,
-            Enabled: req.body.Enabled ? req.body.Enabled : false,
-            Telefono: req.body.Telefono,
-            Rol: Rol.Usuario,
-    };
+    /* Comprobar si el usuario existe previamente */
+    request.get({url:'http://localhost:3000/User/'+req.body.Email}, (error, response, body) => {
 
-    // Petición a la API Rest para crear un nuevo usuario en la bbdd
-    request.post({url: 'http://localhost:3000/User', body: userGiven, json: true,}, (error,response,body) => {
-        
-        if(error || body == '' || !body.Email){
-            error ? resData.message = error : resData.message = "Ha habido un error al procesar el registro.";
-        } else {
-            resData.ok = true;
-            resData.message = "Registro realizado con éxito.";
+        if (error){ // Si la petición arroja un error.
+            error ? resData.message = error : resData.message = 'Ha habido un error al procesar el registro.';
+            res.send(resData);
+        } else if(body != '') { // Si ha devuelto un usuario correctamente, es que existe.
+            resData.message = "El usuario "+req.body.Email+" ya existe. Por favor revise el correo electrónico o inicie sesión."
+            res.send(resData);
+        } else { // Si no hay error pero el cuerpo de la respuesta está vacío, no existe.
+            // Como no existe, proseguimos con el registro.
+            // Contraseña cifrada
+            const hash = bcrypt.hashSync(req.body.Password, 10);
+
+            // Objeto usuario que se va a introducir en la bbdd
+            const userGiven = {
+                    Email: req.body.Email,
+                    Password: hash,
+                    Nombre: req.body.Nombre,
+                    Fecha_Nacimiento: req.body.Fecha_Nacimiento,
+                    Enabled: req.body.Enabled ? req.body.Enabled : false,
+                    Telefono: req.body.Telefono,
+                    Rol: Rol.Usuario,
+            };
+
+            // Petición a la API Rest para crear un nuevo usuario en la bbdd
+            request.post({url: 'http://localhost:3000/User', body: userGiven, json: true,}, (error,response,body) => {
+                
+                if(error || body == '' || !body.Email){
+                    error ? resData.message = error : resData.message = "Ha habido un error al procesar el registro.";
+                } else {
+                    resData.ok = true;
+                    resData.message = "Registro realizado con éxito.";
+                }
+                res.send(resData);
+            });
         }
-        res.send(resData);
-    });   
+        
+    });
 }
